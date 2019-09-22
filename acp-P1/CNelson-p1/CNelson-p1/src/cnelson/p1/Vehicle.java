ae18c5eac800e1958123beb608a3765f5c8f67ca
ecn3 <ecn3@students.uwf.edu>
2019-09-22 14:26:00 -0700

    created vehichle class

--- acp-P1/CNelson-p1/CNelson-p1/src/cnelson/p1/Vehicle.java
+++ acp-P1/CNelson-p1/CNelson-p1/src/cnelson/p1/Vehicle.java
@@ -10,6 +10,7 @@ package cnelson.p1;
  * @author cop4856
  */
 public class Vehicle {
+
     //variables
     private String make;
     private String size;
@@ -17,5 +18,53 @@ public class Vehicle {
     private double engineSize;
     private boolean isImport;
 
-    
+    //defualt constructor created by java
+    public Vehicle(String make, String size, double weight, double engineSize, boolean isImport) {
+        this.make = make;
+        this.size = size;
+        this.weight = weight;
+        this.engineSize = engineSize;
+        this.isImport = isImport;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getEngineSize() {
+        return engineSize;
+    }
+
+    public boolean isIsImport() {
+        return isImport;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setEngineSize(double engineSize) {
+        this.engineSize = engineSize;
+    }
+
+    public void setIsImport(boolean isImport) {
+        this.isImport = isImport;
+    }
+
 }
