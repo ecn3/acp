93999e410168c9a099db864745817ee27f65d45f
ecn3 <ecn3@students.uwf.edu>
2019-09-22 14:17:20 -0700

    created java project in vm

---
+++ acp-P1/CNelson-p1/CNelson-p1/src/cnelson/p1/Vehicle.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cnelson.p1;
+
+/**
+ *
+ * @author cop4856
+ */
+public class Vehicle {
+    //variables
+    private String make;
+    private String size;
+    private double weight;
+    private double engineSize;
+    private boolean isImport;
+
+    
+}
