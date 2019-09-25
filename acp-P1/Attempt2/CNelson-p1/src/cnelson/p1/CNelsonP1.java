d79f5b323f6732b6c89570f0f320f511aec4ef58
ecn3 <ecn3@students.uwf.edu>
2019-09-25 16:00:29 -0700

    sql cant be tested so fuck me I guess

--- acp-P1/Attempt2/CNelson-p1/src/cnelson/p1/CNelsonP1.java
+++ acp-P1/Attempt2/CNelson-p1/src/cnelson/p1/CNelsonP1.java
@@ -1,9 +1,67 @@
 package cnelson.p1;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
 public class CNelsonP1 {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) //if we didnt give it a commandline param database.properties
+        {
+            System.out.println(
+                    "Usage: java -classpath driver_class_path" //derby.jar
+                    + File.pathSeparator
+                    + ". TestDB database.properties");
+            return;
+        } else {
+            System.out.println("args[0] = " + args[0]);
+            SimpleDataSource.init(args[0]);
+        }
+        //1. createConnection()
+        Connection conn = SimpleDataSource.getConnection();
+        Statement stat = conn.createStatement();
+        try {
+            stat.execute("DROP TABLE Test2");
+        } catch (Exception e) {
+            System.out.println("drop failed");
+        }
+
+        try {
+            //2. createTable()
+            stat.execute("CREATE TABLE Test2 (Name CHAR(20),Age INTEGER, Active BOOLEAN)");
+            //3. addDataToTable()
+            stat.execute("INSERT INTO Test2 VALUES ('Romeo',27, true)");
+            stat.execute("INSERT INTO Test2 VALUES ('Juliet',25, true)");
+            stat.execute("INSERT INTO Test2 VALUES ('Tom',64, true)");
+            stat.execute("INSERT INTO Test2 VALUES ('Dick',55, false)");
+            stat.execute("INSERT INTO Test2 VALUES ('Harry',33, true)");
+            
+            //4. issueQuery()
+            ResultSet result = stat.executeQuery("SELECT * FROM Test2");
+
+            System.out.println("after inserts");
+            //5. Process results
+            
+            ResultSetMetaData rsm = result.getMetaData();
+            int cols = rsm.getColumnCount();
+            while (result.next()) {
+                for (int i = 1; i <= cols; i++) {
+                    System.out.print(result.getString(i) + " ");
+                }
+                System.out.println("");
+            }
+            try {
+                stat.execute("DROP TABLE Test2");
+            } catch (Exception e) {
+                System.out.println("drop failed");
+            }
+        } finally {
+            conn.close();
+            System.out.println("dropped Table Test2, closed connection and ending program");
+        }
     }
-    
 }
