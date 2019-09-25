fad6dc343c988d2800c2ca69c977299a8f3cb018
ecn3 <ecn3@students.uwf.edu>
2019-09-25 15:01:41 -0700

    ported simple dataSOurce

--- acp-P1/Attempt2/CNelson-p1/src/cnelson/p1/CNelsonP1.java
+++ acp-P1/Attempt2/CNelson-p1/src/cnelson/p1/CNelsonP1.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cnelson.p1;
 
-/**
- *
- * @author cop4856
- */
 public class CNelsonP1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Hello world");
         
     }
     
