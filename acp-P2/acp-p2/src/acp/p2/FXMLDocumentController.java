946e5281ea6fb7c0424809e5db52ac64e72c5c40
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-10-13 17:53:49 -0500

    base app fxml with scene builder

---
+++ acp-P2/acp-p2/src/acp/p2/FXMLDocumentController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acp.p2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Christian
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
