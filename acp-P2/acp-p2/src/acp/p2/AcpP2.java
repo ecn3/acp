d09ff91b5ee9dfa23323e97e7d0522eaa9fc94eb
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-10-14 17:41:05 -0500

    reads in dict

--- acp-P2/acp-p2/src/acp/p2/AcpP2.java
+++ acp-P2/acp-p2/src/acp/p2/AcpP2.java
@@ -5,6 +5,10 @@
  */
 package acp.p2;
 
+import java.io.File;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,16 +20,26 @@ import javafx.stage.Stage;
  * @author Christian
  */
 public class AcpP2 extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+
         Scene scene = new Scene(root);
-      
+
         stage.setTitle("Spell Checker");
         stage.setScene(scene);
         stage.show();
+
+        Set<String> myDictonary = new HashSet<>();
+
+        Scanner scanner = new Scanner(new File("Words.txt")); //seperate by new line
+
+        while (scanner.hasNext()) {
+            myDictonary.add(scanner.next()); //add each word into the HashSet
+        }
+ 
+
     }
 
     /**
@@ -34,5 +48,5 @@ public class AcpP2 extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
