c654d24b1f9962fea1d7a1b78388396ea03663c3
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-11-11 18:13:15 -0600

    says which is which

--- acp-P4/acp-P4/src/acp/p4/Server.java
+++ acp-P4/acp-P4/src/acp/p4/Server.java
@@ -12,25 +12,36 @@ public class Server {
        public static void main(String[] args) throws IOException {
           ServerSocket ss = new ServerSocket(8081);
           
-          String clientMessage = "";
+          String clientMessage1 = "";
+          String clientMessage2 = "";
           
           Socket s1 = ss.accept(); // takes in a connectio
-          
-          while(!(clientMessage.contains("q"))){
-          System.out.println("Connection made");
+          Socket s2 = ss.accept(); // takes in a connectio
           // recieve messages from client 1
           InputStreamReader in1 = new InputStreamReader(s1.getInputStream());
           BufferedReader bf1 = new BufferedReader(in1);
+            // recieve messages from client 1
+          InputStreamReader in2 = new InputStreamReader(s2.getInputStream());
+          BufferedReader bf2 = new BufferedReader(in2);
           
+          while(!(clientMessage1.contains("q"))){ 
           //get message
-          clientMessage = bf1.readLine();
-          //print mesage
-          System.out.println("Client1 : "+ clientMessage);
+          clientMessage1 = bf1.readLine();
+          //get message
+          clientMessage2 = bf2.readLine();
+          //print mesage c1
+          System.out.println("Client1 : "+ clientMessage1);
+          //print mesage c2
+          System.out.println("Client2 : "+ clientMessage2);
           
           //send message to client1
           PrintWriter pr1 = new PrintWriter(s1.getOutputStream());
-          pr1.println("message to client");
+          pr1.println("Client 2: "+clientMessage2);
           pr1.flush();
+           //send message to client1
+          PrintWriter pr2 = new PrintWriter(s2.getOutputStream());
+          pr2.println("Client 1: "+clientMessage1);
+          pr2.flush();
            }
           
     }
